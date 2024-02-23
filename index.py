80cc3e7a43de16924e80a645cb4cbcbea8ab56a5
Aarya-Veer-Singh <155457557+Aarya-Veer-Singh@users.noreply.github.com>
2024-02-23 16:53:52 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,67 @@
+#Python basics
+
+#Data types - 3
+    #String - set of letters - ex-a,b,c,d.they are between quotation marks("string")
+    #integer - set of number -1,2,3
+    #float - set of numbers with decimal point -1.2,6.453
+
+#Arthimetic operation-works on integer and float
+    #addition (+)
+        1 + 2 
+    #subtraction (-)
+        1 - 2
+    #multiplication(*)
+        1*2
+    #divison (/) -gives quotient and remainder
+        1/2
+    #modulus (%) -gives only remainder
+        2%!
+    #floor divison (//) -gives only the integer of qoutient
+        3//2
+    #exponent (**)
+        3**2
+    #roots (**1/2)
+        4**1/2
+
+#Variables -they are case sensitive
+    #Giving variable a value (words,numbers float)
+        a = 12
+        b = 13
+    #variables can be operated just like numbers example-
+        x = 12
+        y = 3
+        '''x + y = 15
+           x - y  = 9
+           x*y = 36
+           x/y = 4  '''
+    #variables can have string,number and float as value
+        name = yashu
+        age = 10
+        b = 1.23
+
+#input() - takes in input from user
+    #we mostly give the input as a variable 
+       name = input("Enter your name")
+    #we can specify which type of data we want to take ie-string,integer,float.default will allow all
+        age = int(input("enter  your age"))
+
+#print() - prints the value .we can give it string,integer,foat,variable,operation,etc
+    #example
+        print("hello")
+        print(a)
+        print(1**2)
+
+#bolean - true or false
+    #we can check if something is true
+    '''
+    == when  both are equal
+    != when they are not equal
+    <= when right is greater
+    >= when left is greater
+    '''
+
+#functions - pack the operation in one for using multiple times
+def name()
+        Name = aaryaveer
+        print(name)
+        
